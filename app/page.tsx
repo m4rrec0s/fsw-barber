126b62848b576d0d3ee55f411fab59545aa73a95
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-08-01 11:43:57 -0300

    chore: Update global CSS variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-sm uppercase text-gray-200">Home</h1>
+      <Button>Click me</Button>
     </div>
   )
 }
